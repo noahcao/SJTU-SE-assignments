94c57e983ea8da98f39879ba341aaf28e12c7960
NoahCao <noahcao@Lordinateur-de-NoahCao.local>
2017-07-21 17:24:45 +0800

    modify cpp file

--- BPlusTree.cpp
+++ BPlusTree.cpp
@@ -3,47 +3,135 @@
 
 using namespace std;
 
-class Node {
+const int ORDER = 4;  // minnum order of b+ tree
+const int MINNUM_KEY = ORDER - 1; // minnum of key number for a node
+const int MAXNUM_KEY = 2 * ORDER - 1; // maxnum of key number for a node
+const int MINNUM_CHILD = MINNUM_KEY + 1; // minnum of children for a node
+const int MAXNUM_CHILD = MAXNUM_KEY + 1; // maxnum of children for a node
+
+template <typename keyclass>
+class Node{
 public:
-	int sizeOfKey();
-	int numOfChild();
-	Node* getChild(int childIndex);
-	int getKey(int keyIndex);
-	bool isLeaf();
-	vector<int> getKeys();
+	Node(){
+		parent = nullptr;
+		vector<keyclass> keys;
+		vector<Node<keyclass>*> children;
+	}
+	Node(keyclass key, Node<keyclass>* lc = NULL, Node<keyclass>* rc = NULL){
+		vector<keyclass> keys;
+		vector<Node<keyclass>*> children;
+		parent = NULL;
+		keys.insert(0, key);
+		children.insert(0, lc);
+		children.insert(1, rc);
+		if(lc) lc->parent = this;
+		if(rc) rc->parent = this;
+	}
+	bool leaf(){return isLeaf;}
+	vector<keyclass> getKeys(){return keys;}
+	vector<Node<keyclass>*> getChildren(){return children;}
+	int getKeyNum(){return keys.size();}
+	Node<keyclass>* getPar(){return parent;}
 private:
-	vector<Node*> Children;
-	int keyNum;
-	bool isLeaf();
-	vector<int> Keys;
+	bool isLeaf;
+	vector<keyclass> keys;
+	vector<Node<keyclass>*> children;
+	Node<keyclass>* parent;
 };
 
-class Tree {
+template <typename keyclass>
+class Tree{
+protected:
+	int _size;
+	int _order;
+	Node<keyclass>* _root;
+	Node<keyclass>* _hook;
+	void overFlow(Node<keyclass>*);
+	void underFlow(Node<keyclass>*);
 public:
-	Node* getRoot();
-	int getDepth();
-private:
-	Node* root;
-	int depth;
+	Tree(int order = order): _order(order), _size(0)
+	{
+		_root = new Node<keyclass>();
+	}
+	~Tree(){if(root) delete(_root);}
+	int const order(){return _order;}
+	int const size(){return _size;}
+	Node<keyclass>* root(){return _root;}
+	bool empty() const {return !_root;}
+	pair<Node<keyclass>*, int> search(const keyclass& key);
+	bool insert(const keyclass& key);
+	bool remove(const keyclass& key);
 };
 
-pair<Node*, int> search(Node* node, int key) {
-	int i = 1;
-	vector<int> keys = node -> getKeys();
-	while ((i <= node->sizeOfKey) && (key > keys[i])) {
-		i += 1;
+template <typename keyclass>
+pair<Node<keyclass>*, int> Tree<keyclass>::search(const keyclass &key) {
+	Node<keyclass>* node = _root;
+	_hook = NULL;
+	while(node){
+		if(node->leaf()){
+			vector<keyclass> keys = node->getKeys();
+			int keyNum = keys.size();
+			for(int i = 0; i < keyNum; i++) {
+				if (keys[i] == key) return pair(node, i);
+			}
+		}
+		else{
+			vector<keyclass> keys = node->getKeys();
+			int keyNum = keys.size();
+			int childIndex = 0;
+			while(childIndex <= keyNum){
+				if(key < keys[childIndex]){
+					node = node->getChildren()[childIndex];
+					break;
+				}
+				if(childIndex == keyNum){
+					node = node->getChildren()[childIndex];
+					break;
+				}
+				childIndex++;
+			}
+		}
 	}
-	if ((i <= node->sizeOfKey()) && (key == keys[i])) {
-		pair<Node*, int> res(node, i);
-		return res;
-	}
-	else if (node->isLeaf()) {
-		pair<Node*, int> res(nullptr, 0);
-		return res;
-	}
-	else {
-		node = node->getChild(i);
-		return search(node, key);
-	}
+	return NULL;
 }
 
+template <typename keyclass>
+bool Tree<keyclass>::insert(const keyclass &key){
+	Node<keyclass>* node = this->search(key).first;
+	if(node) return false;
+}
+
+template <typename keyclass>
+void Tree<keyclass>::overFlow(Node<keyclass>* node) {
+	if(_order >= node->getChildren().size()) return;
+	int mid = _order / 2;
+	keyclass insertKey = node->getKeys()[mid]
+	Node<keyclass>* new_node = new Node<keyclass>();
+	for(int i = 0; i < _order - mid - 1; i++){
+		new_node->getChildren().insert(i, node->getChildren().remove(mid + i));
+		new_node->getKeys().insert(i, node->getKeys().remove(mid + 1));
+	}
+	new_node->getChildren()[_order - mid - 1] = node->getChildren().remove(s + 1);
+	if(!new_node->getChildren().empty()){
+		int childNum = new_node->getKeyNum() + 1;
+		for(int i = 0; i < childNum; i++) new_node->getChildren()[i]->parent = new_node;
+		// make child of new node's parent the new node
+	}
+	Node<keyclass>* pNode = node->getPar();
+	if(!pNode) {
+		_root = pNode = new Node<keyclass>();
+		pNode->getChildren()[0] = new_node;
+	}
+	int rank = 0;
+	while(rank < pNode->getKeyNum()){
+		if(insertKey < pNode->getKeys()[rank]) {
+			pNode->getKeys().insert(rank, insertKey);
+			node->getKeys().remove(mid);
+			break;
+		}
+		rank++;
+	}
+	pNode->getChildren().insert(rank + 1, new_node);
+	new_node->getPar() = pNode;
+	overFlow(pNode);
+}
