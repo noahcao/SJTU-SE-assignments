98ca5e55c0e01d98fd821266822e4d44becc720b
noahcao <herodan1023@gmail.com>
2017-07-23 21:16:08 +0800

    edit

--- BPlusTree.cpp
+++ BPlusTree.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-const int ORDER = 4;  // order of b+ tree
+const int ORDER = 3;  // order of b+ tree
 // const int MINNUM_KEY = ORDER - 1; // minnum of key number for a node
 // const int MAXNUM_KEY = 2 * ORDER - 1; // maxnum of key number for a node
 // const int MINNUM_CHILD = MINNUM_KEY + 1; // minnum of children for a node
@@ -14,7 +14,7 @@ int searchPos(vector<T> keys, T key) {
     // function to indicate pos of insertion
 	int size = keys.size();
 	for (int i = 0; i < size; i++) {
-		if (key < keys[i]) return i;
+		if (key <= keys[i]) return i;
 	}
 	return size;
 }
@@ -75,9 +75,9 @@ public:
 	int getKeyNum(){return keys.size();}
 	bool isLeaf;
 	vector<T> keys;
-	vector<Node<T>*> children;
 	Node<T>* parent;
-    vector<string> data;
+	vector<string> data;
+	vector<Node<T>*> children;
 };
 
 template<class elem>
@@ -94,6 +94,7 @@ void insertElem(vector<Node<elem>*> &vect, int pos, elem key){
     }
     vect = new_vec;
 }
+
 
 template <typename T>
 class Tree{
@@ -118,9 +119,7 @@ public:
 	pair<Node<T>*, int> search(const T& key);
 	bool insert(const T& key, string Data);
 	bool remove(const T& key);
-    bool recursive_remove(Node<T>* node, T key);
-    void mergeNode(Node<T>* ln, Node<T>* rn);
-    void downFlow(Node<T>* emptyNode);
+    Node<T>* mergeChild(Node<T>* ln, Node<T>* rn);
     void printTree();
     void clear();
 };
@@ -148,12 +147,14 @@ template<typename T>
 void printNode(Node<T>* node){
     if(node->keys.size() == 0) return;
     if(node->isLeaf){
+		cout << "   leaf   ";
         for(int i = 0; i < node->keys.size(); i++){
             cout << node->keys[i] << " ";
         }
         cout << "     ";
     }
     else{
+		cout << "   not leaf   ";
         for(int i = 0; i < node->keys.size(); i++){
             cout << node->keys[i] << " ";
         }
@@ -166,35 +167,32 @@ void printNode(Node<T>* node){
 
 template<typename T>
 void Tree<T>::printTree() {
-    cout << "begin print the tree!";
+	cout << endl;
     if(!_root) return;
     else printNode(_root);
+	cout << endl;
 }
 
 template<typename T>
-void Tree<T>::mergeNode(Node<T> *ln, Node<T> *rn) {
-    // function to merge two neighbor nodes
-    // be default, the right one would be added into the left node
-    if((ln->isLeaf)&&(rn->isLeaf)){
-        // nodes to be merged are both leaf nodes
-        for(int i = 0; i < rn->keys.size(); i++){
-            ln->keys.push_back(rn->keys[i]);
-        }
-        overFlow(ln);
-    }
-    else{
-        // nodes to be merged have children
-        for(int i = 0; i < rn->keys.size(); i++){
-            ln->keys.push_back(rn->keys[i]);
-        }
-        rn->children[0]->parent = ln;
-        mergeNode(ln->children.back(), rn->children[0]);
-        for(int i = 1; i < rn->children.size(); i++){
-            ln->children.push_back(rn->children[i]);
-            rn->children[i]->parent = ln;
-        }
-        overFlow(ln);
-    }
+Node<T>* Tree<T>::mergeChild(Node<T> *ln, Node<T> *rn) {
+    // function to merge two neighbor children
+	if (ln->isLeaf) {
+		for (int i = 0; i < rn->keys.size(); i++) {
+			ln->keys.push_back(rn->keys[i]);
+			ln->data.push_back(rn->data[i]);
+		}
+	return ln;
+	}
+	else {
+		Node<T>* mergeNode = mergeChild(ln->children.back(), rn->children[0]);
+		removeElem(rn->children, 0);
+		for (int i = 0; i < rn->keys.size(); i++){
+			ln->keys.push_back(rn->keys[i]);
+			ln->children.push_back(rn->children[i]);
+			rn->children[i]->parent = ln;
+		}
+		return mergeNode;
+	}
 }
 
 template <typename T>
@@ -209,20 +207,21 @@ pair<Node<T>*, int> Tree<T>::search(const T &key) {
 			for(int i = 0; i < keyNum; i++) {
 				if (keys[i] == key) return pair<Node<T>*, int>(node, i);
 			}
+			return pair<Node<T>*, int>(NULL, -1);
 		}
 		else{
 			vector<T> keys = node->keys;
 			int keyNum = keys.size();
 			int Index = 0;
-			while(Index <= keyNum){
+			while(Index < keyNum){
 				if(key < keys[Index]){
                     _hook = node;
 					node = node->children[Index];
 					break;
 				}
-				if(Index == keyNum){
+				if(Index == keyNum - 1){
                     _hook = node;
-					node = node->children[Index];
+					node = node->children[Index + 1];
 					break;
 				}
 				Index++;
@@ -234,13 +233,13 @@ pair<Node<T>*, int> Tree<T>::search(const T &key) {
 
 template <typename T>
 bool Tree<T>::insert(const T &key, string Data){
-    cout << "sssss";
 	pair<Node<T>*, int> target = this->search(key);
-    cout << "sssss";
     Node<T>* node = target.first;
     if(node) return false;
+	node = new Node<T>(key, Data);
     if(_hook == NULL){
         // case that leaf is root
+		node = _root;
         insertElem(node->keys, searchPos(node->keys, key), key);
         insertElem(node->data, searchPos(node->data, Data), Data);
         overFlow(node);
@@ -259,112 +258,73 @@ bool Tree<T>::insert(const T &key, string Data){
 
 template <typename T>
 void Tree<T>::overFlow(Node<T>* node) {
-	if(_order >= node->children.size()) return;
-	int mid = _order / 2;
+	if (_order >= node->keys.size()) return;
+	int mid = (_order + 1) / 2;
 	T insertKey = node->keys[mid];
 	Node<T>* new_node = new Node<T>();
     if(node->isLeaf){
         // case 1: node is leaf, the mid key should be copied to parent node
-        for(int i = 0; i < _order - mid; i++){
-            new_node->keys.push_back(node->keys[mid + i]);
+		new_node->isLeaf = true;
+        //for(int i = 0; i < _order - mid + 1; i++){
+		int size = node->keys.size();
+		for (int i = 0; i < size - mid; i++) {
+            new_node->keys.push_back(node->keys[mid]);
+			new_node ->data.push_back(node->data[mid]);
             removeElem(node->keys, mid);
+			removeElem(node->data, mid);
         }
-        new_node->parent = node->parent;
-        new_node->isLeaf = true;
-        int index = searchPos(node->parent->keys, insertKey);
-        insertElem(node->parent->keys, index, insertKey);
-        insertElem(node->parent->children, index + 1, new_node);
-        overFlow(node->parent);
+		if (node->parent) {
+			new_node->parent = node->parent;
+			int index = searchPos(node->parent->keys, insertKey);
+			insertElem(node->parent->keys, index, insertKey);
+			insertElem(node->parent->children, index + 1, new_node);
+			overFlow(node->parent);
+		}
+		else {
+			Node<T>* new_root = new Node<T>();
+			_root = new_root;
+			_root->children.push_back(node);
+			_root->children.push_back(new_node);
+			_root->keys.push_back(insertKey);
+			node->parent = _root;
+			new_node->parent = _root;
+			return;
+		}
     }
     if(!node->isLeaf){
         // case 2: node is not leaf, only promote the mid key to parent node
-        for(int i = 0; i < _order - mid - 1; i++){
+		int size = node->keys.size();
+        //for(int i = 0; i < _order - mid; i++){
+		for(int i = 0; i < size - mid - 1; i++){
             new_node->children.push_back(node->children[mid + 1]);
+			node->children[mid + 1]->parent = new_node;
             new_node->keys.push_back(node->keys[mid + 1]);
             removeElem(node->children, mid + 1);
             removeElem(node->keys, mid + 1);
         }
         new_node->children.push_back(node->children.back());
-        new_node->parent = node->parent;
+		node->children.back()->parent = new_node;
         removeElem(node->children, mid + 1);
-        removeElem(node->keys, mid);
-        int index = searchPos(node->keys, insertKey);
-        insertElem(node->parent->keys, index, insertKey);
-        insertElem(node->parent->children, index + 1, new_node);
-        overFlow(node->parent);
+        removeElem(node->keys, mid);	// remove the insert key from former node
+		if (node == _root) {
+			// case when node is the root (has no parent)
+			Node<T>* new_root = new Node<T>();
+			_root = new_root;
+			new_node->parent = _root;
+			node->parent = _root;
+			_root->keys.push_back(insertKey);
+			_root->children.push_back(node);
+			_root->children.push_back(new_node);
+		}
+		else {
+			// case when node has a parent
+			new_node->parent = node->parent;
+			int index = searchPos(node->parent->keys, insertKey);
+			insertElem(node->parent->keys, index, insertKey);
+			insertElem(node->parent->children, index + 1, new_node);
+			overFlow(node->parent);
+		}
     }
-	vector<T> new_keys;
-	vector<Node<T>*> new_children;
-	for(int i = 0; i <= mid + 1; i++){
-		new_keys.push_back(node->keys[i]);
-		new_children.push_back(node->children[i]);
-	}
-}
-
-template<typename T>
-bool Tree<T>::recursive_remove(Node<T>* node, T key){
-    // function to delete a key in one node and ancestor nodes
-    // note that input node has parent
-    if(node->keys.size() == 0){
-        downFlow(node);
-        recursive_remove(node, key);
-    }
-    else{
-        for(int i = 0; i < node->keys.size(); i++){
-            if(node->keys[i] == key){
-                removeElem(node->keys, i);
-                mergeNode(node->children[i], node->children[i + 1]);
-                removeElem(node->children, i + 1);
-                return true;
-            }
-        }
-        if(node->parent)    recursive_remove(node->parent, key);
-        else return true;
-    }
-}
-
-template <typename T>
-void Tree<T>::downFlow(Node<T>* emptyNode){
-    if(!emptyNode->parent) return;
-    if(emptyNode->keys.size() > 0) return;
-    int rank = 0;
-    while(emptyNode->parent->children[rank] != emptyNode) rank++;
-    // get the index of empty node in this parent's children
-    if(rank == 0){
-        Node<T>* rs = emptyNode->parent->children[1];
-        if(emptyNode->isLeaf){
-            if(rs->keys.size() > 1){
-                emptyNode->keys.push_back(emptyNode->parent->keys[0]);
-                emptyNode->parent->keys[0] = rs->keys[0];
-                removeElem(rs->keys, 0);
-            }
-            else{
-                removeElem(emptyNode->parent->children, 0);
-                removeElem(emptyNode->keys, 0);
-                emptyNode = rs;
-                downFlow(emptyNode->parent);
-            }
-        }
-        else{
-            if(rs->keys.size() > 1){
-                emptyNode->parent->keys[0] = rs->keys[1];
-                emptyNode->keys.push_back(rs->keys[0]);
-                emptyNode->children[1] = rs->children[1];
-                removeElem(rs->children, 1);
-                removeElem(rs->keys, 0);
-            }
-            else{
-                mergeNode(emptyNode->children[0], rs->children[0]);
-                rs->children[0] = emptyNode->children[0];
-                emptyNode->children[0].parent = rs;
-                emptyNode = rs;
-                removeElem(emptyNode->parent->children, 0);
-                removeElem(emptyNode->parent->keys, 0);
-                overFlow(emptyNode->children[0]);
-            }
-        }
-    }
-
 }
 
 template <typename T>
@@ -373,53 +333,103 @@ bool Tree<T>::remove(const T& key) {
 	Node<T>* node = target.first;
 	int index = target.second;
 	if (!node) return false;
+	// if key exists, then node should be a leaf 
     // case 0: node is the root (tree with height 1)
     if (node == _root){
         removeElem(node->keys, index);
+		removeElem(node->data, index);
         return true;
     }
     // case 1: index != 0
     if (index != 0){
         removeElem(node->keys, index);
+		removeElem(node->data, index);
+		underFlow(node);
         return true;
     }
-    if (index == 0) {
-        if (node == node->parent->children[0]) {
-            // case 2: index == 0, node is the first child of parent;
-            // and node has more than one key
-            removeElem(node->keys, 0);
-            recursive_remove(node, key);
-        }
-        if (node != node->parent->children[0]) {
-            // case 3: index == 0, node isn't the first child of parent;
-            if(node->keys.size() > 1){
-                int rank = searchPos(node->parent->keys, key);
-                node->parent->keys[rank] = node->keys[1];
-                removeElem(node->keys, 0);
-            }
-            else{
-                int rank = searchPos(node->parent->keys, key);
-                removeElem(node->parent->keys, rank);
-                removeElem(node->parent->children, rank + 1);
-                downFlow(node->parent);
-                delete(node);
-            }
-        }
+	if (index == 0) {
+		if (node == node->parent->children[0]) {
+			// case 2: index == 0, node is the first child of parent;
+			// and node has more than one key
+			removeElem(node->keys, 0);
+			removeElem(node->data, 0);
+			Node<T>* formerNode = node;
+			// Node<T>* upHolder = searchUpHolder(key);
+			// if node is the first child of parent, then its ancesstor may also
+			// have the same key
+			while (node == node->parent->children[0]) {
+				if (node->parent == _root) {
+					underFlow(formerNode);
+					return true;
+				}
+				else node = node->parent;
+			}
+			node = node->parent;
+			int index = searchPos(node->keys, key);
+			if (node == _root) {
+				if (node->keys.size() == 1) {
+					Node<T>* flow_node = mergeChild(node->children[0], node->children[1]);
+					node->children[0]->parent = NULL;
+					_root = node->children[0];
+					while (flow_node != _root) {
+						overFlow(flow_node);
+						flow_node = flow_node->parent;
+					}
+				}
+				else {
+					removeElem(node->keys, index);
+					Node<T>* flow_node = mergeChild(node->children[index], node->children[index + 1]);
+					removeElem(node->children, index + 1);
+					while (flow_node != _root) {
+						overFlow(flow_node);
+						flow_node = flow_node->parent;
+					}
+				}
+			}
+			else {
+				removeElem(node->keys, index);
+				Node<T>* flow_node = mergeChild(node->children[index], node->children[index + 1]);
+				removeElem(node->children, index + 1);
+				while (flow_node != _root) {
+					overFlow(flow_node);
+					flow_node = flow_node->parent;
+				}
+				underFlow(node);
+			}
+			/*if (upHolder != NULL){
+				removeHolder(upHolder, key);
+				underFlow(upHolder);
+			}*/
+			underFlow(formerNode);
+		}
+		else{
+			// case 3: index == 0, node isn't the first child of parent;
+			int rank = searchPos(node->parent->keys, key);
+			node->parent->keys[rank] = node->keys[1];
+			removeElem(node->keys, 0);
+			removeElem(node->data, 0);
+			underFlow(node);
+			return true;
+		}
     }
 }
 
 template <typename T>
 void Tree<T>::underFlow(Node<T> * node) {
-	if((_order + 1) / 2 <= node->children.size()) return;
+	if ((_order + 1) / 2 <= node->keys.size() + 1) {
+		return;
+	}
 	Node<T>* pNode = node->parent;
 	if(!pNode){
-		if((node->keys.size()==0)&&(node->children[0])){
-			// case that the root (node) has no key already but has a sole child
-			// which is not empty
-			_root = node->children[0];
-			_root->parent = NULL;
-			node->children[0] = NULL;
-			delete node;
+		if (!node->isLeaf) {
+			if ((node->keys.size() == 0) && (node->children[0])) {
+				// case that the root (node) has no key already but has a sole child
+				// which is not empty
+				_root = node->children[0];
+				_root->parent = NULL;
+				node->children[0] = NULL;
+				delete node;
+			}
 		}
 		return;
 	}
@@ -430,12 +440,18 @@ void Tree<T>::underFlow(Node<T> * node) {
 		// if node is not the first child of pNode
 		// then node certainly has a left subling
 		Node<T>* ls = pNode->children[rank - 1];
-		if((_order + 1) / 2 < ls->children.size()){
+		if((_order + 1) / 2 < ls->keys.size() + 1){
 			insertElem(node->keys, 0, pNode->keys[rank - 1]);
 			pNode->keys[rank - 1] = ls->keys[ls->keys.size() - 1];
 			removeElem(ls->keys, ls->keys.size() - 1);
-			insertElem(node->children, 0, ls->children[ls->children.size() - 1]);
-			removeElem(ls->children, ls->children.size() - 1);
+			if (ls->isLeaf) {
+				insertElem(node->data, 0, ls->data.back());
+				removeElem(ls->data, ls->data.size() - 1);
+			}
+			else {
+				insertElem(node->children, 0, ls->children[ls->children.size() - 1]);
+				removeElem(ls->children, ls->children.size() - 1);
+			}
 			if(!node->isLeaf) node->children[0]->parent = node;
 			return;
 		}
@@ -445,13 +461,21 @@ void Tree<T>::underFlow(Node<T> * node) {
         // if node is not the last child of pNode
         // then node certainly has a right subling
         Node<T> *rs = pNode->children[rank + 1];
-        if ((_order + 1) / 2 < rs->children.size()) {
+        if ((_order + 1) / 2 < rs->keys.size() + 1) {
             node->keys.push_back(pNode->keys[rank]);
-            pNode->keys[rank] = rs->keys[0];
-            removeElem(rs->keys, 0);
-            node->children.push_back(rs->children[0]);
-            removeElem(rs->children, 0);
-            if(node->children[node->children.size() - 1]) {
+			if (!node->isLeaf) {
+				pNode->keys[rank] = rs->keys[0];
+				removeElem(rs->keys, 0);
+				node->children.push_back(rs->children[0]);
+				removeElem(rs->children, 0);
+			}
+			else {
+				pNode->keys[rank] = rs->keys[1];
+				node->data.push_back(rs->data[0]);
+				removeElem(rs->keys, 0);
+				removeElem(rs->data, 0);
+			}
+            if(!node->isLeaf) {
                 node->children.back()->parent = node;
             }
             return;
@@ -466,18 +490,22 @@ void Tree<T>::underFlow(Node<T> * node) {
         ls->keys.push_back(pNode->keys[rank - 1]);
         removeElem(pNode->keys, rank - 1);
         removeElem(pNode->children, rank);
-        ls->children.push_back(node->children[0]);
-        if(ls->children[ls->children.size() - 1]){
-            ls->children.back()->parent = ls;
-        }
+		if (!node->isLeaf) {
+			ls->children.push_back(node->children[0]);
+			ls->children.back()->parent = ls;
+		}
         while(!node->keys.empty()){
             ls->keys.push_back(node->keys[0]);
             removeElem(node->keys, 0);
-            ls->children.push_back(node->children[0]);
-            removeElem(node->children, 0);
-            if(ls->children[ls->children.size() - 1]){
-                ls->children[ls->children.size() - 1]->parent = ls;
-            }
+			if (!node->isLeaf) {
+				ls->children.push_back(node->children[0]);
+				removeElem(node->children, 0);
+				ls->children.back()->parent = ls;
+			}
+			else {
+				ls->data.push_back(node->data[0]);
+				removeElem(node->data, 0);
+			}
         }
         delete(node);
     }
@@ -487,11 +515,11 @@ void Tree<T>::underFlow(Node<T> * node) {
         insertElem(rs->keys, 0, pNode->keys[rank]);
         removeElem(pNode->keys, rank);
         removeElem(pNode->children, rank);
-        insertElem(rs->children, 0, node->children.back());
-        removeElem(node->children, node->children.size() - 1);
-        if(rs->children[0]){
-            rs->children[0]->parent = rs;
-        }
+		if (!node->isLeaf) {
+			insertElem(rs->children, 0, node->children.back());
+			removeElem(node->children, node->children.size() - 1);
+			rs->children[0]->parent = rs;
+		}
         while(!node->keys.empty()){
             insertElem(rs->keys, 0, node->keys.back());
             removeElem(node->keys, node->keys.size() - 1);
@@ -509,10 +537,15 @@ void Tree<T>::underFlow(Node<T> * node) {
 
 int main() {
     Tree<int> tree;
-    cout << "dadaa" << endl;
-    cout << "dadaa" << endl;
-    tree.insert(2, "2");
-    cout << "dadaa" << endl;
-    tree.printTree();
-    return 0;
+	for (int i = 1; i < 35 ; i++) {
+		tree.insert(i, "1");
+	}
+	tree.printTree();
+	tree.remove(3);
+	tree.remove(1);
+	tree.remove(13);
+	tree.printTree();
+	tree.remove(19);
+	tree.printTree();
+	system("pause");
 }
