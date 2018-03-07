6ff596b9d29be2c9d458742aedae9f0fc293767a
noahcao <herodan1023@gmail.com>
2018-03-07 23:40:50 +0800

    add the additional functions

--- wordladder/Main.java
+++ wordladder/Main.java
@@ -14,8 +14,12 @@ import java.util.Stack;
 
 public class Main {
 
+    static HashSet<String> dict  = new HashSet<String>();
+    static HashSet<String> usedWord = new HashSet<String>();
+    static Queue<Stack<String>> path = new LinkedList<Stack<String>>();
+    static Stack<String> topStack = new Stack<String>();
     public static void main(String[] args) throws IOException{
-        HashSet<String> dict  = new HashSet<String>();
+        //HashSet<String> dict  = new HashSet<String>();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter path of dictionary file:");
         String fileName = sc.nextLine();
@@ -25,94 +29,90 @@ public class Main {
             dict.add(contentLine);
             contentLine = br.readLine();
         }
-        System.out.print("Enter start word of word ladder:");
-        String start = sc.nextLine();;
-        System.out.print("Enter end word of word ladder:");;
-        String end = sc.nextLine();
+        while(true){
+            usedWord = new HashSet<String>();
+            path = new LinkedList<Stack<String>>();
+            System.out.print("Enter start word of word ladder (print Enter to quit):");
+            String start = sc.nextLine();
+            if(start.equals("")) break;
+            System.out.print("Enter end word of word ladder:");
+            String end = sc.nextLine();
+            WordLadder(end, start, dict);
+        }
         sc.close();
-        WordLadder(start, end, dict);
-        System.out.println(ladderLength(start, end, dict));
     }
 
     public static  void WordLadder(String start, String end, HashSet<String> dict){
-        HashSet<String> usedWord = new HashSet<String>();
-        Queue<Stack<String>> path = new LinkedList<Stack<String>>();
+        //HashSet<String> usedWord = new HashSet<String>();
+        //Queue<Stack<String>> path = new LinkedList<Stack<String>>();
         Stack<String> wordStack = new Stack<String>();
         usedWord.add(start);
         wordStack.push(start);
         path.offer(wordStack);
         while(!path.isEmpty()){
-            System.out.println("a stack in path");
-            Stack<String> topStack = path.poll();
+            topStack = path.poll();
             if(!topStack.isEmpty()){
-                System.out.println("a word in stack");
                 String topWord = topStack.peek();
                 int wordSize = topWord.length();
                 for(int i = 0; i < wordSize; i++){
-                    char[] wordArray = topWord.toCharArray();
+                    String tempWord;
+                    boolean finish = false;
+                    //char[] wordArray = topWord.toCharArray();
                     for(char c = 'a'; c <= 'z'; c++){
-                        if(wordArray[i] == c) continue;
-                        else{
-                            char[] tempWordArr = wordArray;
-                            tempWordArr[i] = c;
-                            String tempWord = new String(tempWordArr);
-                            if(tempWord.equals(end)){
-                                topStack.push(tempWord);
-                                while(!topStack.isEmpty()){
-                                    String currWord = topStack.pop();
-                                    System.out.print(currWord + ' ');
-                                }
-                                System.out.println("");
-                                return;
-                            }
-                            else{
-                                if(dict.contains(tempWord)){
-                                    if(!usedWord.contains(tempWord)){
-                                        usedWord.add(tempWord);
-                                        Stack<String> copiedStack = (Stack<String>)topStack.clone();
-                                        copiedStack.push(tempWord);
-                                        path.offer(copiedStack);
-                                    }
-                                }
-                            }
+                        // 1. delete a character
+                        if(wordSize > 1){
+                            tempWord = topWord.substring(0,i) + topWord.substring(i+1,wordSize);
+                            finish = checkWord(tempWord, topStack, end);
+                            if(finish) return;
                         }
+                        // 2. add a character
+                        if(i == wordSize - 1){
+                            tempWord = topWord.substring(0, wordSize) + c;
+                            finish = checkWord(tempWord, topStack, end);
+                            if(finish) return;
+                        }
+                        else{
+                            tempWord = topWord.substring(0,i) + c + topWord.substring(i+1, wordSize);
+                            finish = checkWord(tempWord, topStack, end);
+                            if(finish) return;
+                        }
+                        // 3. replace a character
+                        tempWord = topWord.substring(0,i) + c + topWord.substring(i+1,wordSize);
+                        finish = checkWord(tempWord, topStack, end);
+                        if(finish) return;
                     }
                 }
             }
         }
-        System.out.println("finish");
+        System.out.println("finish without a word ladder found");
     }
 
-    public static int ladderLength(String start, String end, HashSet<String> dict){
-        if (dict.size() == 0){
-            return 0;
+    public static void printLadder(Stack<String> topStack){
+        // print words building the word ladder
+        while(!topStack.isEmpty()){
+            String currWord = topStack.pop();
+            System.out.print(currWord + ' ');
         }
-        LinkedList<String> wordQueue = new LinkedList<String>();
-        LinkedList<Integer> distanceQueue = new LinkedList<Integer>();
+        System.out.println("");
+    }
 
-        wordQueue.add(start);
-        distanceQueue.add(1);
-
-        while(!wordQueue.isEmpty()){
-            String currWord = wordQueue.pop();
-            Integer currDistance = distanceQueue.pop();
-            if(currWord.equals(end)){
-                return currDistance;
-            }
-            for(int i = 0; i < currWord.length(); i++){
-                char[] currCharArr = currWord.toCharArray();
-                for(char c='a'; c<='z'; c++){
-                    currCharArr[i] = c;
-
-                    String newWord = new String(currCharArr);
-                    if(dict.contains(newWord)){
-                        wordQueue.add(newWord);
-                        distanceQueue.add(currDistance+1);
-                        dict.remove(newWord);
-                    }
+    public static boolean checkWord(String tempWord, Stack<String>topStack, String end){
+        // check whether tempWord == end, and finish or continue depending on the result
+        if(tempWord.equals(end)){
+            topStack.push(tempWord);
+            printLadder(topStack);
+            return true;
+        }
+        else{
+            if(dict.contains(tempWord)){
+                if(!usedWord.contains(tempWord)){
+                    usedWord.add(tempWord);
+                    Stack<String> copiedStack = (Stack<String>)topStack.clone();
+                    copiedStack.push(tempWord);
+                    path.offer(copiedStack);
                 }
             }
+            return false;
         }
-        return 0;
     }
 }
