7adea853a4ba7a28361eaf797ae9b9176bc72f03
noahcao <herodan1023@gmail.com>
2018-03-19 08:37:27 +0800

    input jquery and bootstrap config

--- OnlineBookShop/build/webpack.base.conf.js
+++ OnlineBookShop/build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -78,5 +79,13 @@ module.exports = {
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "windows.jQuery": "jquery"
+    })
+  ]
 }
+
--- OnlineBookShop/src/main.js
+++ OnlineBookShop/src/main.js
@@ -1,5 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
